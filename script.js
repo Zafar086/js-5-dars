1dff191d0d2afeec7edb4ff9d296a158f79d2f55
Zafar086 <ergashevzafar1777@gmail.com>
2025-04-29 15:16:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const first = prompt("Ismingizni kiriting");
+
+let second = prompt("Space idingizni kiriting");
+
+let third = prompt("Space passwordingizni kiriting");
+
+let zafar = {
+  name: first,
+  id: second,
+  password: third,
+};
+
+console.log(zafar)
